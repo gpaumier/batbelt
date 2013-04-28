766d93bdca8641ee4fe0b2c2433d883d2bd66ca0
Guillaume Paumier <guillom.pom@gmail.com>
2013-04-28 11:26:06 +0300

    rm dupe script tag

--- stats.js
+++ stats.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
   var _paq = _paq || [];
   _paq.push(["trackPageView"]);
   _paq.push(["enableLinkTracking"]);
@@ -10,4 +9,3 @@
     var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
     g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
   })();
-</script>
